e48eccd0d363cee666a303af5bb28eedce92feb9
MdFarzan <mdfarzan776@gmail.com>
2022-10-04 10:41:22 +0530

    admin-panel ui improved

--- application/views/admin/partials/header.php
+++ application/views/admin/partials/header.php
@@ -11,6 +11,8 @@
   <link rel="stylesheet" href="<?php echo base_url('assets/plugins/fontawesome-free/css/all.min.css'); ?>">
   <!-- Theme style -->
   <link rel="stylesheet" href="<?php echo base_url('assets/dist/css/adminlte.min.css'); ?>">
+  <!-- Theme Customization -->
+  <link rel="stylesheet" href="<?php echo base_url('assets/styles/adminlte.css'); ?>">
 </head>
 <body class="hold-transition sidebar-mini">
 <div class="wrapper">
@@ -23,7 +25,9 @@
         <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
       </li>
       <li class="nav-item d-none d-sm-inline-block">
-        <a href="<?php echo site_url(); ?>" class="nav-link">Back to site</a>
+        <a href="#" class="nav-link text-dark">
+          Hello, <?php echo $this->session->userdata('ADMIN_NAME')?$this->session->userdata('ADMIN_NAME'):'Super Admin'; ?>
+        </a>
       </li>
       
     </ul>
@@ -34,12 +38,35 @@
         <a class="nav-link" data-widget="fullscreen" title="Full Screen" href="#" role="button">
           <i class="fas fa-expand-arrows-alt"></i>
         </a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" title="Logout" href="<?php echo base_url('/admin/logout') ?>" role="button">
-          <!-- <i class="fas fa-th-large"></i> -->
-          <i class="fas fa-sign-out-alt"></i>
+      </li>      
+      <!-- Profile Dropdown Menu -->
+      <li class="nav-item dropdown">
+        <a class="nav-link" data-toggle="dropdown" href="#">
+          <i class="fas fa-user"></i>
         </a>
+        <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right p-1">
+
+          <div class="profile-avtar">
+            <img src="http://localhost/quizzer/assets/dist/img/user2-160x160.jpg" class="img-fluid" alt="profile" /> 
+          </div>
+          <span class="h-6 dropdown-item dropdown-header">
+            <h5 class="mb-1 lead admin-title">Hello, <?php echo $this->session->userdata('ADMIN_NAME') !=null ?$this->session->userdata('ADMIN_NAME'): 'Super Admin' ?></h5>
+          </span>
+          <div class="dropdown-divider"></div>
+          <a href="#" class="dropdown-item">
+          <i class="fas fa-user-cog mr-2"></i> Profile Setting
+          </a>
+          <div class="dropdown-divider"></div>
+          <a href="#" class="dropdown-item">
+            <i class="fas fa-cogs mr-2"></i></i> Site Setting
+          </a>
+          <div class="dropdown-divider"></div>
+          <a href="<?php echo base_url(); ?>" class="dropdown-item">
+            <i class="fas fa-arrow-left mr-2"></i> Back to Site
+          </a>
+          <div class="dropdown-divider"></div>
+          <a href="<?php echo base_url('/admin/logout'); ?>" class="btn btn-warning dropdown-item dropdown-footer">LOGOUT</a>
+        </div>
       </li>
     </ul>
 
