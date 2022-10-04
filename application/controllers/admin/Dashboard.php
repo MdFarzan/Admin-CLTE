6b9d6580bd5c9593dc1d6ef596e7d722530a8337
MdFarzan <mdfarzan776@gmail.com>
2022-10-04 10:56:17 +0530

    dashboad menu added in sidebar

--- application/controllers/admin/Dashboard.php
+++ application/controllers/admin/Dashboard.php
@@ -15,10 +15,12 @@ class Dashboard extends MY_Controller{
     public function index(){
         
         $this->page_data['page_title'] = 'Dashboard';
+        $this->page_data['parent_menu'] = null;
+        $this->page_data['current_menu'] = 'dashboard';
 
         $this->load->view('admin/partials/header.php', $this->page_data);
-        $this->load->view('admin/partials/sidenav.php');
-        $this->load->view('admin/dashboard.php');
+        $this->load->view('admin/partials/sidenav.php', $this->page_data);
+        $this->load->view('admin/dashboard.php', $this->page_data);
         $this->load->view('admin/partials/footer.php');
     }
 
--- application/views/admin/partials/sidenav.php
+++ application/views/admin/partials/sidenav.php
@@ -35,8 +35,17 @@
         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
           <!-- Add icons to the links using the .nav-icon class
                with font-awesome or any other icon font library -->
+          <li class="nav-item">
+            <a href="<?php echo base_url('/admin/dashboard'); ?>"
+               class="nav-link <?php echo $current_menu=='dashboard'?' active':''; ?>">
+              <i class="nav-icon fas fa-tachometer-alt"></i>
+              <p>
+                Dashboard
+              </p>
+            </a>
+          </li>
           <li class="nav-item menu-open">
-            <a href="#" class="nav-link active">
+            <a href="#" class="nav-link">
               <i class="nav-icon fas fa-tachometer-alt"></i>
               <p>
                 Starter Pages
